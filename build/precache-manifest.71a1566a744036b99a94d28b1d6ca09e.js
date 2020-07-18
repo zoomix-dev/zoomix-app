d62c4d64d80ae5305f30e1786e98672ca61ed90e
Luis Barrientos <luis.barrientos@takeit.co>
2020-07-18 17:53:39 -0400

    add build process

---
+++ build/precache-manifest.71a1566a744036b99a94d28b1d6ca09e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1b9d6f90e3b2a6fc7e23d45b2c19c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0931d8f9511cc3ffc99d",
+    "url": "/static/css/main.5eb538be.chunk.css"
+  },
+  {
+    "revision": "cb9623c74273a7514da1",
+    "url": "/static/js/2.f2dc6faa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2dc6faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0931d8f9511cc3ffc99d",
+    "url": "/static/js/main.4232f8d3.chunk.js"
+  },
+  {
+    "revision": "67e9b8cce9a73de3918e",
+    "url": "/static/js/runtime-main.6abb2996.js"
+  }
+]);
